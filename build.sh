6b2a431e44aa9c4d78f1c452d0f4e5ba2f523edb
azzam2912 <azzam.gamers12@gmail.com>
2025-03-15 00:04:47 +0700

    refactored table of contents

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Compile main.tex twice to generate table of contents
+pdflatex main.tex
+pdflatex main-problems.tex
+pdflatex main-solutions.tex
+
 pdflatex main.tex
 pdflatex main-problems.tex
 pdflatex main-solutions.tex
